24b64641ad7a5a70b4a70f8044e247cfeac29c7f
Daniel Park <parkdk96@gmail.com>
2019-10-17 10:00:12 -0700

    Doug

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,6 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  validates :name, presence: true, uniqueness: true
+  validates :breed, :age, presence: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :first_name, :last_name, presence: true, uniqueness: true
+  validates :title, :alias, presence: true, uniqueness: true
+
+  def to_s
+    self.first_name + " " + self.last_name
+  end
+
+  def dog_name
+    self.dog.name
+  end
+
 end
